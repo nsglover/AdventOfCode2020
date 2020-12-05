0a80aaf1b0b3e4e2b3efb91d68c688419b529c05
Nathan Glover <nathanielsglover@gmail.com>
2020-12-05 01:06:42 -0500

    Solve day 5

--- Puzzles/Day5.hs
+++ Puzzles/Day5.hs
@@ -1,8 +1,24 @@
 module Puzzles.Day5 (Day) where
 
 import Common.Solution
+import Data.List
 
 data Day = Day
 instance Solve Day where
-  part1 _ ls = show 5
-  part2 _ ls = show 5
+  part1 _ ls = show $ foldl1 max $ map (seatId . position) ls
+  part2 _ ls = show $ findSeat $ sort $ map (seatId . position) ls
+
+seatId (r, c) = 8 * r + c
+
+position :: String -> (Int, Int)
+position = position' (0, 127) (0, 7)
+  where position' (loR, hiR) (loC, hiC) [] = (hiR, hiC)
+        position' (loR, hiR) c ('F':xs) = position' (loR, loR + (hiR - loR) `div` 2) c xs
+        position' (loR, hiR) c ('B':xs) = position' (loR + (hiR - loR) `div` 2, hiR) c xs
+        position' r (loC, hiC) ('L':xs) = position' r (loC, loC + (hiC - loC) `div` 2) xs
+        position' r (loC, hiC) ('R':xs) = position' r (loC + (hiC - loC) `div` 2, hiC) xs
+
+findSeat :: [Int] -> Int
+findSeat [] = error "No empty seats"
+findSeat [x] = x
+findSeat (x:y:xs) = if x + 1 == y then findSeat (y:xs) else x + 1
